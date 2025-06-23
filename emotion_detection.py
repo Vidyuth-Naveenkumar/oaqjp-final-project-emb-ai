e130bbaae34203f039e144a6ce26ce946e20341f
Vidyuth-Naveenkumar <vidyuthcaslv2010@gmail.com>
2025-06-23 14:15:45 -0400

    update

--- emotion_detection.py
+++ emotion_detection.py
@@ -1,15 +1,31 @@
 import requests
+import json
+  
+def emotion_detector(text_to_analyse):  
+    url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'  
+    myobj = { "raw_document": { "text": text_to_analyse } } 
+    header = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"} 
+    
+    response = requests.post(url, json=myobj, headers=header) 
+    formatted_response = json.loads(response.text)
 
-def emotion_detector(text_to_analyse):
-    url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
-    header = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
-    myobj = { "raw_document": { "text": text_to_analyse } }
-    response = requests.post(url, json = myobj, headers=header)
-    return response.text
+    # Emotional values
+    emotional_data = formatted_response['emotionPredictions'][0]['emotionMentions'][0]['emotion']
+    
+    anger_score = emotional_data['anger']
+    sadness_score = emotional_data['sadness']
+    joy_score = emotional_data['joy']
+    disgust_score = emotional_data['disgust']
+    fear_score = emotional_data['fear']
 
+    # Fixed typo here
+    dominant_emotion = max(emotional_data, key=emotional_data.get)
 
-
-
-
-
-
+    return {
+        'anger': anger_score,
+        'disgust': disgust_score,
+        'fear': fear_score,
+        'joy': joy_score,
+        'sadness': sadness_score,
+        'dominant_emotion': dominant_emotion
+    }
